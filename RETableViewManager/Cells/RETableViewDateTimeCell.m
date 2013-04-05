0e27d2e0748829a2b738a78420d4b90a76721eae
Roman Efimov <romaonthego@gmail.com>
2013-04-05 16:57:46 -0500

    Expose additional date picker properties

--- RETableViewManager/Cells/RETableViewDateTimeCell.m
+++ RETableViewManager/Cells/RETableViewDateTimeCell.m
@@ -31,25 +31,33 @@
 - (void)cellDidLoad
 {
     [super cellDidLoad];
-    _datePicker = [[UIDatePicker alloc] init];
-    _dateFormatter = [[NSDateFormatter alloc] init];
-    [_datePicker addTarget:self action:@selector(datePickerValueDidChange:) forControlEvents:UIControlEventValueChanged];
     self.selectionStyle = self.tableViewManager.style.defaultCellSelectionStyle;
-}
-
-- (void)cellWillAppear
-{
-    _textField = [[UITextField alloc] initWithFrame:CGRectNull];
     
+    _textField = [[UITextField alloc] initWithFrame:CGRectNull];
     _textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     _textField.inputAccessoryView = self.actionBar;
     _textField.delegate = self;
     [self addSubview:_textField];
     
+    _dateFormatter = [[NSDateFormatter alloc] init];
+    
+    _datePicker = [[UIDatePicker alloc] init];
+    [_datePicker addTarget:self action:@selector(datePickerValueDidChange:) forControlEvents:UIControlEventValueChanged];
+}
+
+- (void)cellWillAppear
+{
     self.textLabel.text = self.item.title;
     self.textField.inputView = _datePicker;
+    
     _datePicker.date = self.item.value;
     _datePicker.datePickerMode = self.item.datePickerMode;
+    _datePicker.locale = self.item.locale;
+    _datePicker.calendar = self.item.calendar;
+    _datePicker.timeZone = self.item.timeZone;
+    _datePicker.minimumDate = self.item.minimumDate;
+    _datePicker.maximumDate = self.item.maximumDate;
+    _datePicker.minuteInterval = self.item.minuteInterval;
     
     _dateFormatter.dateFormat = self.item.format;
     self.detailTextLabel.text = [_dateFormatter stringFromDate:self.item.value];
--- RETableViewManager/Items/REDateTimeItem.h
+++ RETableViewManager/Items/REDateTimeItem.h
@@ -12,7 +12,15 @@
 
 @property (strong, readwrite, nonatomic) NSDate *value;
 @property (strong, readwrite, nonatomic) NSString *format;
-@property (assign, readwrite, nonatomic) UIDatePickerMode datePickerMode;
+@property (assign, readwrite, nonatomic) UIDatePickerMode datePickerMode; // default is UIDatePickerModeDateAndTime
+
+@property (strong, readwrite, nonatomic) NSLocale *locale;                // default is [NSLocale currentLocale]. setting nil returns to default
+@property (copy, readwrite, nonatomic) NSCalendar *calendar;              // default is [NSCalendar currentCalendar]. setting nil returns to default
+@property (strong, readwrite, nonatomic) NSTimeZone *timeZone;              // default is nil. use current time zone or time zone from calendar
+
+@property (strong, readwrite, nonatomic) NSDate *minimumDate;           // specify min/max date range. default is nil. When min > max, the values are ignored. Ignored in countdown timer mode
+@property (strong, readwrite, nonatomic) NSDate *maximumDate;           // default is nil
+@property (assign, readwrite, nonatomic) NSInteger minuteInterval;        // display minutes wheel with interval. interval must be evenly divided into 60. default is 1. min is 1, max is 30
 
 + (id)itemWithTitle:(NSString *)title value:(NSDate *)value format:(NSString *)format;
 + (id)itemWithTitle:(NSString *)title value:(NSDate *)value format:(NSString *)format datePickerMode:(UIDatePickerMode)datePickerMode;
