8beb2dea88a63c23598ef6193657142a6da2d17b
Roman Efimov <romaonthego@gmail.com>
2013-06-04 10:39:42 -0500

    Add inital data to section (collapsed state)

--- RETableViewManagerExample/RETableViewManagerExample/Classes/Controllers/RetractableViewController.m
+++ RETableViewManagerExample/RETableViewManagerExample/Classes/Controllers/RetractableViewController.m
@@ -42,6 +42,8 @@
     
     [collapsedItems addObjectsFromArray:@[@"Test item 1", @"Test item 2", @"Test item 3", showMoreItem]];
     [expandedItems addObjectsFromArray:@[@"Test item 1", @"Test item 2", @"Test item 3", @"Test item 4", @"Test item 5", @"Test item 6", showLessItem]];
+    
+    [_section addItemsFromArray:collapsedItems];
 }
 
 @end
