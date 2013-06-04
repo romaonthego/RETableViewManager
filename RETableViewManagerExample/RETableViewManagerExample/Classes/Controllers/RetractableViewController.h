d67fc04f7ec02ecac7f67f1f43ca129f712f4e27
Roman Efimov <romaonthego@gmail.com>
2013-06-04 10:15:50 -0500

    Add RetractableViewController

---
+++ RETableViewManagerExample/RETableViewManagerExample/Classes/Controllers/RetractableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RetractableViewController.h
+//  RETableViewManagerExample
+//
+//  Created by Roman Efimov on 6/4/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RetractableViewController : UITableViewController
+
+@end
---
+++ RETableViewManagerExample/RETableViewManagerExample/Classes/Controllers/RetractableViewController.m
@@ -0,0 +1,23 @@
+//
+//  RetractableViewController.m
+//  RETableViewManagerExample
+//
+//  Created by Roman Efimov on 6/4/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import "RetractableViewController.h"
+
+@interface RetractableViewController ()
+
+@end
+
+@implementation RetractableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end
