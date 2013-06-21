1963634207df11a564884a764bfae57a994981b1
Roman Efimov <romefimov@gmail.com>
2013-06-21 17:47:30 -0400

    Update RETableViewManager.h

--- RETableViewManager/RETableViewManager.h
+++ RETableViewManager/RETableViewManager.h
@@ -63,7 +63,7 @@
 @property (strong, readwrite, nonatomic) NSMutableArray *sections;
 
 /**
- The `UITableView` that needs to be managed.
+ The `UITableView` that needs to be managed using this `RETableViewManager`.
  */
 @property (weak, readwrite, nonatomic) UITableView *tableView;
 
