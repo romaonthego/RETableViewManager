766b8b24bd6d54fe8cde82d78394b4a9d52fc847
Roman Efimov <romaonthego@gmail.com>
2013-07-19 16:27:56 -0500

    Add comment

--- RETableViewManager/RETableViewManager.m
+++ RETableViewManager/RETableViewManager.m
@@ -153,6 +153,9 @@ NSUInteger REDeviceSystemMajorVersion() {
         cell = [[cellClass alloc] initWithStyle:cellStyle reuseIdentifier:cellIdentifier];
         cell.tableViewManager = self;
         [cell cellDidLoad];
+        
+        // RETableViewManagerDelegae
+        //
         if ([_delegate respondsToSelector:@selector(tableView:willLoadCell:forRowAtIndexPath:)])
             [_delegate tableView:tableView willLoadCell:cell forRowAtIndexPath:indexPath];
     }
