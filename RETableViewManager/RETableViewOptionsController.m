4e78dafccaf4e4117d165fadfb470c6ff2708a81
Roman Efimov <romaonthego@gmail.com>
2013-09-04 11:02:05 -0500

    Use properties instead of iVars in RETableViewOptionsController.m

--- RETableViewManager/RETableViewOptionsController.m
+++ RETableViewManager/RETableViewOptionsController.m
@@ -28,6 +28,9 @@
 
 @interface RETableViewOptionsController ()
 
+@property (strong, readwrite, nonatomic) RETableViewManager *tableViewManager;
+@property (strong, readwrite, nonatomic) RETableViewSection *mainSection;
+
 @end
 
 @implementation RETableViewOptionsController
@@ -51,12 +54,12 @@
 {
     [super viewDidLoad];
 
-    _tableViewManager = [[RETableViewManager alloc] initWithTableView:self.tableView delegate:self.delegate];
-    _mainSection = [[RETableViewSection alloc] init];
-    [_tableViewManager addSection:_mainSection];
+    self.tableViewManager = [[RETableViewManager alloc] initWithTableView:self.tableView delegate:self.delegate];
+    self.mainSection = [[RETableViewSection alloc] init];
+    [self.tableViewManager addSection:self.mainSection];
     
     if (self.style)
-        _tableViewManager.style = self.style;
+        self.tableViewManager.style = self.style;
     
     __typeof (&*self) __weak weakSelf = self;
     void (^refreshItems)(void) = ^{
@@ -84,7 +87,7 @@
                 }
             }
         }
-        [_mainSection addItem:[RETableViewItem itemWithTitle:title accessoryType:accessoryType selectionHandler:^(RETableViewItem *selectedItem) {
+        [self.mainSection addItem:[RETableViewItem itemWithTitle:title accessoryType:accessoryType selectionHandler:^(RETableViewItem *selectedItem) {
             UITableViewCell *cell = [weakSelf.tableView cellForRowAtIndexPath:selectedItem.indexPath];
             if (!weakSelf.multipleChoice) {
                 for (NSIndexPath *indexPath in [weakSelf.tableView indexPathsForVisibleRows]) {
