cf6613d2a4c7b8fe08b484d08a4d05954e733045
Roman Efimov <romaonthego@gmail.com>
2013-09-09 09:49:23 -0500

    Fix bool item cell text label dimensions, close #30

--- RETableViewManager/Cells/RETableViewBoolCell.m
+++ RETableViewManager/Cells/RETableViewBoolCell.m
@@ -64,6 +64,8 @@
 - (void)layoutSubviews
 {
     [super layoutSubviews];
+    self.textLabel.frame = CGRectMake(self.textLabel.frame.origin.x, self.textLabel.frame.origin.y, self.textLabel.frame.size.width - self.switchView.frame.size.width - self.section.style.contentViewMargin - 10.0, self.textLabel.frame.size.height);
+    self.textLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     if ([self.tableViewManager.delegate respondsToSelector:@selector(tableView:willLayoutCellSubviews:forRowAtIndexPath:)])
         [self.tableViewManager.delegate tableView:self.tableViewManager.tableView willLayoutCellSubviews:self forRowAtIndexPath:[(UITableView *)self.superview indexPathForCell:self]];
 }
