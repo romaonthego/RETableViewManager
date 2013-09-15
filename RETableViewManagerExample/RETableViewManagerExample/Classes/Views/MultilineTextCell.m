9186c701ecee90c60d9af1a8bd978647b8372066
Roman Efimov <romaonthego@gmail.com>
2013-09-14 19:56:52 -0500

    Multiline label tweaks

--- RETableViewManagerExample/RETableViewManagerExample/Classes/Views/MultilineTextCell.m
+++ RETableViewManagerExample/RETableViewManagerExample/Classes/Views/MultilineTextCell.m
@@ -22,7 +22,11 @@ static const CGFloat kVerticalMargin = 10.0;
 
 + (CGFloat)heightWithItem:(MultilineTextItem *)item tableViewManager:(RETableViewManager *)tableViewManager
 {
-    CGFloat width = CGRectGetWidth(tableViewManager.tableView.bounds) - 2.0 * kHorizontalMargin;
+    CGFloat horizontalMargin = kHorizontalMargin;
+    if (REDeviceIsUIKit7() && item.section.style.contentViewMargin <= 0)
+        horizontalMargin += 5.0;
+    
+    CGFloat width = CGRectGetWidth(tableViewManager.tableView.bounds) - 2.0 * horizontalMargin;
     return [item.title sizeWithFont:[UIFont systemFontOfSize:17] constrainedToSize:CGSizeMake(width, INFINITY)].height + 2.0 * kVerticalMargin;
 }
 
@@ -33,12 +37,14 @@ static const CGFloat kVerticalMargin = 10.0;
     self.multilineLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
     self.multilineLabel.font = [UIFont systemFontOfSize:17];
     self.multilineLabel.numberOfLines = 0;
+    self.multilineLabel.backgroundColor = [UIColor clearColor];
     [self.contentView addSubview:self.multilineLabel];
 }
 
 - (void)cellWillAppear
 {
     [super cellWillAppear];
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
     self.textLabel.text = @"";
     self.multilineLabel.text = self.item.title;
 }
@@ -47,8 +53,12 @@ static const CGFloat kVerticalMargin = 10.0;
 {
     [super layoutSubviews];
     
+    CGFloat horizontalMargin = kHorizontalMargin;
+    if (REDeviceIsUIKit7() && self.section.style.contentViewMargin <= 0)
+        horizontalMargin += 5.0;
+    
     CGRect frame = CGRectMake(0, 0, CGRectGetWidth(self.contentView.bounds), [MultilineTextCell heightWithItem:self.item tableViewManager:self.tableViewManager]);
-    frame = CGRectInset(frame, kHorizontalMargin, kVerticalMargin);
+    frame = CGRectInset(frame, horizontalMargin, kVerticalMargin);
     
     self.multilineLabel.frame = frame;
 }
