6eb91980b4f65354cb58f1f2f79027564afe072a
Roman Efimov <romaonthego@gmail.com>
2013-09-21 10:20:36 -0500

    Fix unused function warning, issue #48

--- RETableViewManager/RECommonFunctions.h
+++ RETableViewManager/RECommonFunctions.h
@@ -27,23 +27,7 @@
 
 #ifndef REUIKitIsFlatModeFunction
 #define REUIKitIsFlatModeFunction
-static BOOL REUIKitIsFlatMode()
-{
-    static BOOL isUIKitFlatMode = NO;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        if (floor(NSFoundationVersionNumber) > 993.0) {
-            // If your app is running in legacy mode, tintColor will be nil - else it must be set to some color.
-            if (UIApplication.sharedApplication.keyWindow) {
-                isUIKitFlatMode = [UIApplication.sharedApplication.delegate.window performSelector:@selector(tintColor)] != nil;
-            } else {
-                // Possible that we're called early on (e.g. when used in a Storyboard). Adapt and use a temporary window.
-                isUIKitFlatMode = [[UIWindow new] performSelector:@selector(tintColor)] != nil;
-            }
-        }
-    });
-    return isUIKitFlatMode;
-}
+BOOL REUIKitIsFlatMode();
 #endif
 
 BOOL REDeviceIsUIKit7() __attribute__ ((deprecated));
--- RETableViewManager/RECommonFunctions.m
+++ RETableViewManager/RECommonFunctions.m
@@ -29,3 +29,21 @@ BOOL REDeviceIsUIKit7()
 {
     return REUIKitIsFlatMode();
 }
+
+BOOL REUIKitIsFlatMode()
+{
+    static BOOL isUIKitFlatMode = NO;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if (floor(NSFoundationVersionNumber) > 993.0) {
+            // If your app is running in legacy mode, tintColor will be nil - else it must be set to some color.
+            if (UIApplication.sharedApplication.keyWindow) {
+                isUIKitFlatMode = [UIApplication.sharedApplication.delegate.window performSelector:@selector(tintColor)] != nil;
+            } else {
+                // Possible that we're called early on (e.g. when used in a Storyboard). Adapt and use a temporary window.
+                isUIKitFlatMode = [[UIWindow new] performSelector:@selector(tintColor)] != nil;
+            }
+        }
+    });
+    return isUIKitFlatMode;
+}
