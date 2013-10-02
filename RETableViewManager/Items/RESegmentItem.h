b3f3f97af222003f49e41b724d1b0e7df8d9cbb0
Roman Efimov <romaonthego@gmail.com>
2013-10-02 15:35:48 -0500

    Add Dmitry Shmidt copyright notice, reference #82

--- RETableViewManager/Items/RESegmentItem.h
+++ RETableViewManager/Items/RESegmentItem.h
@@ -2,7 +2,8 @@
 // RESegmentItem.h
 // RETableViewManager
 //
-// Copyright (c) 2013 Roman Efimov (https://github.com/romaonthego)
+// Copyright (c) 2013 Dmitry Shmidt (https://github.com/shmidt)
+//                    Roman Efimov (https://github.com/romaonthego)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
