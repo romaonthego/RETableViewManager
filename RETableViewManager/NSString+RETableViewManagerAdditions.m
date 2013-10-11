97cf8dc05fe42141784f462a42e6fcea921b1f3c
Roman Efimov <romaonthego@gmail.com>
2013-10-11 09:52:49 -0500

    Add conditions to fix compilers warnings under iOS 6 sdk

--- RETableViewManager/NSString+RETableViewManagerAdditions.m
+++ RETableViewManager/NSString+RETableViewManagerAdditions.m
@@ -32,14 +32,16 @@
 {
     CGSize size;
     if ([self respondsToSelector:@selector(sizeWithAttributes:)]) {
-        NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:@selector(sizeWithAttributes:)];
-        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
-        [invocation setTarget:self];
-        [invocation setSelector:@selector(sizeWithAttributes:)];
-        NSDictionary *attributes = @{ NSFontAttributeName:font };
-        [invocation setArgument:&attributes atIndex:2];
-        [invocation invoke];
-        [invocation getReturnValue:&size];
+        IF_IOS7_OR_GREATER(
+            NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:@selector(sizeWithAttributes:)];
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+            [invocation setTarget:self];
+            [invocation setSelector:@selector(sizeWithAttributes:)];
+            NSDictionary *attributes = @{ NSFontAttributeName:font };
+            [invocation setArgument:&attributes atIndex:2];
+            [invocation invoke];
+            [invocation getReturnValue:&size];
+        );
     } else {
         NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:@selector(sizeWithFont:)];
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
@@ -56,21 +58,23 @@
 {
     CGSize resultSize;
     if ([self respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)]) {
-        NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:@selector(boundingRectWithSize:options:attributes:context:)];
-        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
-        [invocation setTarget:self];
-        [invocation setSelector:@selector(boundingRectWithSize:options:attributes:context:)];
-        NSDictionary *attributes = @{ NSFontAttributeName:font };
-        NSStringDrawingOptions options = NSStringDrawingUsesLineFragmentOrigin;
-        NSStringDrawingContext *context;
-        [invocation setArgument:&size atIndex:2];
-        [invocation setArgument:&options atIndex:3];
-        [invocation setArgument:&attributes atIndex:4];
-        [invocation setArgument:&context atIndex:5];
-        [invocation invoke];
-        CGRect rect;
-        [invocation getReturnValue:&rect];
-        resultSize = rect.size;
+        IF_IOS7_OR_GREATER(
+            NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:@selector(boundingRectWithSize:options:attributes:context:)];
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+            [invocation setTarget:self];
+            [invocation setSelector:@selector(boundingRectWithSize:options:attributes:context:)];
+            NSDictionary *attributes = @{ NSFontAttributeName:font };
+            NSStringDrawingOptions options = NSStringDrawingUsesLineFragmentOrigin;
+            NSStringDrawingContext *context;
+            [invocation setArgument:&size atIndex:2];
+            [invocation setArgument:&options atIndex:3];
+            [invocation setArgument:&attributes atIndex:4];
+            [invocation setArgument:&context atIndex:5];
+            [invocation invoke];
+            CGRect rect;
+            [invocation getReturnValue:&rect];
+            resultSize = rect.size;
+        );
     } else {
         NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:@selector(sizeWithFont:constrainedToSize:)];
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
