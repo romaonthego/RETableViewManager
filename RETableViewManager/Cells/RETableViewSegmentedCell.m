712d1cdb4dfe71568456668f4bed55c85753adf1
Roman Efimov <romaonthego@gmail.com>
2013-10-24 09:28:15 -0500

    Fix superview reference bug in iOS 7

--- RETableViewManager/Cells/RETableViewSegmentedCell.m
+++ RETableViewManager/Cells/RETableViewSegmentedCell.m
@@ -94,7 +94,7 @@
     self.textLabel.frame = CGRectMake(self.textLabel.frame.origin.x, self.textLabel.frame.origin.y, self.textLabel.frame.size.width - self.segmentedControl.frame.size.width - self.section.style.contentViewMargin - 10.0, self.textLabel.frame.size.height);
     self.textLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     if ([self.tableViewManager.delegate respondsToSelector:@selector(tableView:willLayoutCellSubviews:forRowAtIndexPath:)])
-        [self.tableViewManager.delegate tableView:self.tableViewManager.tableView willLayoutCellSubviews:self forRowAtIndexPath:[(UITableView *)self.superview indexPathForCell:self]];
+        [self.tableViewManager.delegate tableView:self.tableViewManager.tableView willLayoutCellSubviews:self forRowAtIndexPath:self.item.indexPath];
 }
 
 #pragma mark -
