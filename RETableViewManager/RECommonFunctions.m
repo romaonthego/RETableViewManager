81b3465029c5c1a8566855123f473a94cf224eff
Roman Efimov <romefimov@gmail.com>
2014-03-11 09:03:03 -0700

    Fix iOS 7.1 flat mode detection bug, close #150

--- RETableViewManager/RECommonFunctions.m
+++ RETableViewManager/RECommonFunctions.m
@@ -38,10 +38,10 @@ BOOL REUIKitIsFlatMode()
         if (floor(NSFoundationVersionNumber) > 993.0) {
             // If your app is running in legacy mode, tintColor will be nil - else it must be set to some color.
             if (UIApplication.sharedApplication.keyWindow) {
-                isUIKitFlatMode = [UIApplication.sharedApplication.delegate.window performSelector:@selector(tintColor)] != nil;
+                isUIKitFlatMode = [UIApplication.sharedApplication.delegate.window respondsToSelector:@selector(tintColor)];
             } else {
                 // Possible that we're called early on (e.g. when used in a Storyboard). Adapt and use a temporary window.
-                isUIKitFlatMode = [[UIWindow new] performSelector:@selector(tintColor)] != nil;
+                isUIKitFlatMode = [[UIWindow new] respondsToSelector:@selector(tintColor)];
             }
         }
     });
